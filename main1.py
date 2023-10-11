725d6eee8f2ef8ee5065cbcbfaad74178a1f5550
Juan Pablo <jpsilvernova@gmail.com>
2023-10-11 12:48:16 -0600

    first commit

---
+++ main1.py
@@ -0,0 +1,21 @@
+import turtle
+
+# Create canvas instance
+myturtle = turtle.Turtle()
+
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+
+turtle.done()
+
+
+
+
+
+
+
